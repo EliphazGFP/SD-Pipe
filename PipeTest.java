5fbcbce8235044bcd139c48da33f9439e409b043
EliphazGFP <eliphazgabriel.volcano@outlook.com>
2025-12-10 09:17:30 -0300

    Update PipeTest.java

--- PipeTest.java
+++ PipeTest.java
@@ -1,29 +1,30 @@
-import java.util.*;
 import java.io.*;
 
-public class PipeTest
-{  public static void main(String args[])
-   {  try
-      {  /* set up pipes */
-         PipedOutputStream pout1 = new PipedOutputStream();
-         PipedInputStream pin1 = new PipedInputStream(pout1);
+public class PipeTest {
+    public static void main(String[] args) throws IOException {
+        System.out.println("=== PRODUTOR → FILTRO → CONSUMIDOR com PIPES ===\n");
 
-         PipedOutputStream pout2 = new PipedOutputStream();
-         PipedInputStream pin2 = new PipedInputStream(pout2);
+        // Primeiro pipe: Produtor → Filtro
+        PipedOutputStream pout1 = new PipedOutputStream();
+        PipedInputStream pin1 = new PipedInputStream(pout1);
+        DataOutputStream producerOut = new DataOutputStream(pout1);
+        DataInputStream filterIn = new DataInputStream(pin1);
 
-         /* construct threads */
+        // Segundo pipe: Filtro → Consumidor
+        PipedOutputStream pout2 = new PipedOutputStream();
+        PipedInputStream pin2 = new PipedInputStream(pout2);
+        DataOutputStream filterOut = new DataOutputStream(pout2);
+        DataInputStream consumerIn = new DataInputStream(pin2);
 
-         Producer prod = new Producer(pout1);
-         Filter filt = new Filter(pin1, pout2);
-         Consumer cons = new Consumer(pin2);
+        // Criar e iniciar as threads
+        Producer p = new Producer(producerOut);
+        Filter f = new Filter(filterIn, filterOut);
+        Consumer c = new Consumer(consumerIn);
 
-         /* start threads */
+        p.start();
+        f.start();
+        c.start();
 
-         prod.start();
-         filt.start();
-         cons.start();
-      }
-      catch (IOException e){}
-   }
+        System.out.println("Todas as threads iniciadas. Deixe rodar por alguns segundos e pressione CTRL+C para parar.\n");
+    }
 }
-
